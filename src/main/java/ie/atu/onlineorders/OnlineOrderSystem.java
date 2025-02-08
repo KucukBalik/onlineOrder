9398a90c0fa4ae3a95b0b6c0d2a034224492cad0
Emre Demir <g00427391@atu.ie>
2025-02-08 16:08:40 +0000

    array is created

--- src/main/java/ie/atu/onlineorders/OnlineOrderSystem.java
+++ src/main/java/ie/atu/onlineorders/OnlineOrderSystem.java
@@ -1,16 +1,21 @@
 package ie.atu.onlineorders;
 
 import java.awt.*;
+import java.util.ArrayList;
 
 public class OnlineOrderSystem {
 
     public static void main(String[] args) {
-        MenuItems[] items = new MenuItems[] {
 
-                items[1] = new Burgers("Cheese", 15.00, "Tomato");
+        ArrayList<MenuItems> items = new ArrayList<>();
 
+        items.add(new Burgers("CheeseBurger", 5.99, "Cheese, Ketchup"));
+        items.add(new Pizza("Pepperoni Pizza", 8.99, "Pepperoni, Cheese, Tomato Sauce"));
+        items.add(new Salad("Caesar Salad", 6.49, "Lettuce, Croutons, Caesar Dressing"));
+
+        for (MenuItems item : items) {
+            System.out.println(item.toString());
         }
-
 
     }
 }
